a5a08b24b1e40449aeb360d4ba173aa7068faa7b
Ester Oliveira <oliver.ester.2004@gmail.com>
2024-03-19 20:19:17 -0300

    criando aplicação de enquetes

---
+++ curso/src/main/java/com/esteroliver/enquetes/EnquetesApplication.java
@@ -0,0 +1,11 @@
+package com.esteroliver.enquetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnquetesApplication {
+    public static void main(String[] args){
+        SpringApplication.run(EnquetesApplication.class, args);
+    }
+}
